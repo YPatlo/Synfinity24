3534e36ed4959aad0b9c195506bec1539a93aeb2
Yashwanth P <patloyashwanthkumar2@gmail.com>
2024-09-20 08:53:18 +0000

    Added DB Url

--- synfinity/synfinity/urls.py
+++ synfinity/synfinity/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,3 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("synfinity24.urls"))
 ]
+
+urlpatterns = staticfiles_urlpatterns()
